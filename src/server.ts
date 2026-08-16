85d93336c372fea187712bd98fdf7c24bb1e2713
unknown <samiraborsha08@gmail.com>
2026-08-16 17:26:14 +0600

    feat: add DB connection, env config, and graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app";
+import { Server } from "http";
+import { envVars } from "./app/config/env";
+
+let server: Server;
 
 const startServer = async () => {
     try {
-        app.listen(5000, () => {
-            console.log(`✅ Server is listening on port 5000`);
+        await mongoose.connect(envVars.DB_URL);
+        console.log("✅ Connected to Database");
+
+        server = app.listen(envVars.PORT, () => {
+            console.log(`✅ Server is listening on port ${envVars.PORT}`);
         });
     } catch (error) {
         console.log(`❌ Error from server`, error);
@@ -11,3 +19,51 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received... Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+
+    process.exit(1);
+});
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received... Server shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+
+    process.exit(1);
+});
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Server shutting down...", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+
+    process.exit(1);
+});
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught Exception Detected... Server shutting down...", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+
+    process.exit(1);
+});
